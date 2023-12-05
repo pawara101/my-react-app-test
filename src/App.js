74909f53edec6121012156886e8a0f2a549d9987
pawara101 <pawara101dassanayake@gmail.com>
2023-12-05 22:09:54 +0530

    2023-12-05 | react application tutorial 1.0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  // State variable to hold the fetched data
+  const [data, setData] = useState([]);
+
+  // URL for the API endpoint
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  // Function to fetch data from the API
+  const fetchInfo = function () {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d));
+  };
+
+  // useEffect hook to fetch data when the component mounts
+  useEffect(() => {
+    console.log('Fetching data...');
+    fetchInfo();
+  }, []);
+
+  // JSX rendering of the component
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
+      <center>
+        {data.map((dataObj, index) => (
+          <div
+            key={index} // Adding a unique key to each rendered element
+            style={{
+              width: "15em",
+              backgroundColor: "#35D841",
+              padding: 2,
+              borderRadius: 10,
+              marginBlock: 10,
+            }}
+          >
+            <p style={{ fontSize: 20, color: 'white' }}>{dataObj.name}</p>
+          </div>
+        ))}
+      </center>
     </div>
   );
 }
+
 
 export default App;
