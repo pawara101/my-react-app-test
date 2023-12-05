b9219b15efba6f57adc1645ad190137d7554a1ef
pawara101 <pawara101dassanayake@gmail.com>
2023-12-06 00:53:44 +0530

    2023-12-05|React trials|Display simple JSX

--- src/App.js
+++ src/App.js
@@ -3,57 +3,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 
-function App() {
-  // State variable to hold the fetched data
-  const [data, setData] = useState([]);
-
-  // URL for the API endpoint
-  const url = "https://jsonplaceholder.typicode.com/users";
-
-  // Function to fetch data from the API
-  const fetchInfo = function () {
-    return fetch(url)
-      .then((res) => res.json())
-      .then((d) => setData(d));
-  };
-
-  // useEffect hook to fetch data when the component mounts
-  useEffect(() => {
-    console.log('Fetching data...');
-    fetchInfo();
-  }, []);
-
-  // JSX rendering of the component
-  // return (
-  //   <div className="App">
-  //     <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
-  //     <center>
-  //       {data.map((dataObj, index) => (
-  //         <div
-  //           key={index} // Adding a unique key to each rendered element
-  //           style={{
-  //             width: "15em",
-  //             backgroundColor: "#35D841",
-  //             padding: 2,
-  //             borderRadius: 10,
-  //             marginBlock: 10,
-  //           }}
-  //         >
-  //           <p style={{ fontSize: 20, color: 'black' }}>{dataObj.name}</p>
-  //         </div>
-  //       ))}
-  //     </center>
-  //   </div>
-  // );
-
-return(
-  <div>
-    {data.map(user =>(
-      <li key={user.id}>{user.name}</li>
-    ))}
-  </div>
-);
-}
-
+function App() {}
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,9 @@
+const TodoApp = () =>{
+    return(
+        <div>
+            <h1>Hello world!!!</h1>
+            <p>I am in areact Component!!</p>
+        </div>
+    );
+};
+export default TodoApp;
