1e62e2061cea6454332dfd0d66367fab22b97b6f
pawara101 <pawara101dassanayake@gmail.com>
2023-12-06 01:11:52 +0530

    2023-12-06|React trials|Display all users to the browser

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 
-function App() {
-    return (<div>JSX is cool!</div>);
-}
+// Use array mapping in React
+const users = [
+    { name: "John Doe", id: 1 },
+    { name: "Jane Doe", id: 2 },
+    { name: "Billy Doe", id: 3 }
+  ];
+  
+  function App() {
+    return (
+      <>
+        <h3>User names</h3>
+        <ul>
+            {users.map(user=>(
+                <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+      </>
+    );
+  }
 
 export default App;
