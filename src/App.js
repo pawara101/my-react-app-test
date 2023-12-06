828211e9ea2744f63f71cc0b58ce899130e9cdfe
pawara101 <pawara101dassanayake@gmail.com>
2023-12-07 02:19:15 +0530

    2023-12-07|React Trials|Sum of Two Numbers.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-/**
-  Challenge: Make the button functional
-  A click on button should toggle (show/hide) the string `Toggle Challenge` each time it is pressed**/
   
   function App() {
-    const [value, setValue] = React.useState("");
+    const [number1, setNumber1] = React.useState();
+    const [number2, setNumber2] = React.useState();
+    const [total, setTotal] = React.useState(0);
+
+    function calculateTotal() {
+      setTotal(number1 + number2);
+    }
 
     return (
-      <>
-        <input
-          type="text"
-          placeholder="Enter Text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <p>{value}</p>
-      </>
+      <div>
+        <h2>Adding Two Numbers</h2>
+        <input placeholder="First Number" type="number" val1={number1} onChange={(e) => setNumber1(+e.target.value)}/>
+        <input placeholder="Second Number" type="number" val2={number1} onChange={(e) => setNumber2(+e.target.value)}/>
+        <button onClick={calculateTotal}>Add Two Numbers</button>
+        <p>Total: {total || ""}</p>
+      </div>
     );
   }
   
