719630bdf2f8d8b597d7ef9f4c188dedfbad83e8
pawara101 <pawara101dassanayake@gmail.com>
2023-12-07 01:52:14 +0530

    2023-12-07|React Trials|2 way data binding in ReactJS

--- src/App.js
+++ src/App.js
@@ -7,26 +7,20 @@ import React, { useState, useEffect } from 'react';
   A click on button should toggle (show/hide) the string `Toggle Challenge` each time it is pressed**/
   
   function App() {
-    const [toggle, setToggle] = React.useState(true);
-    const [clickCount, setClickCount] = React.useState(0); //Declares two pieces of state using the useState hook
-    
-    const handleToggle = () =>{
-      
-      setToggle(!toggle);
-      setClickCount(clickCount+1);
-      console.log("click ",{clickCount},toggle)
-      
-     
-    };
+    const [value, setValue] = React.useState("");
 
-    
     return (
       <>
-        <button type='button' onClick={handleToggle}>Hide Element Below</button>
-  
-        <div id='div1' style={{ display: toggle ? 'block' : 'none' }}>Toggle Challenge</div>
+        <input
+          type="text"
+          placeholder="Enter Text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <p>{value}</p>
       </>
     );
   }
+  
 
 export default App;
